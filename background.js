191463577c9c9f1cb4e57e1f6b7dc189363580e9
Serendipitous1729 <123492768+Serendipitous1729@users.noreply.github.com>
2023-02-03 15:35:22 +0530

    Add files via upload
    version 2

--- background.js
+++ background.js
@@ -20,6 +20,15 @@ async function setSyncStorage(key, value){
     queryObject[key] = value;
     chrome.storage.sync.set(queryObject);
 }
+var blockYoutube = true;
+// const youtubeBlockedSites = [
+//     "https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ",
+//     "https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig",
+//     "https://www.youtube.com/gaming",
+//     "https://www.youtube.com/channel/UCYfdidRxbB8Qhf0Nx7ioOYw",
+//     "https://www.youtube.com/channel/UCEgdi0XIXXZ-qJOFPf4JSKw",
+//     "https://www.youtube.com/channel/UCrpQ4p1Ql_hG8rKXIKM1MOQ"
+// ];
 (async function(){
     var blockedSites = await getSyncStorage("blockedSites");
     if(blockedSites == undefined){
@@ -27,13 +36,26 @@ async function setSyncStorage(key, value){
             "https://www.facebook.com/",
             "https://twitter.com/",
             "https://www.instagram.com/",
-            "https://web.snapchat.com/",
-            "https://www.youtube.com/shorts"
+            "https://web.snapchat.com/"
         ]);
     }
     let focusMode = await getSyncStorage("focusMode");
     if(focusMode == undefined){
         await setSyncStorage("focusMode", false);
+    }
+
+    let favourites = await getSyncStorage("favourites");
+    if(favourites == undefined){
+        await setSyncStorage("favourites", [
+            { name: "Google Search", href: "https://www.google.com/"},
+            { name: "Gmail", href: "https://mail.google.com/mail/u/0/#inbox"},
+            { name: "YouTube", href: "https://www.youtube.com" },
+            { name: "Discord", href: "https://www.discord.com" }
+        ]);
+    }
+    let todoEntries = await getSyncStorage("todoEntries");
+    if(todoEntries == undefined){
+        await setSyncStorage("todoEntries", ["Add your pending tasks here!"]);
     }
 })();
 
@@ -48,6 +70,26 @@ chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab){
             if(tab.url.slice(0, blockedSites[i].length) == blockedSites[i]){
                 // Checks if the first part of the page url is a blocked site
                 chrome.tabs.update(tabId, {url: "chrome://newtab"});
+            }
+        }
+        // Check YouTube sites
+        if(blockYoutube && tab.url.slice(0, 24) == "https://www.youtube.com/"){
+            // for(var i = 0; i < youtubeBlockedSites.length; i++){
+            //     if(tab.url.slice(0, youtubeBlockedSites[i].length) == youtubeBlockedSites[i]){
+            //         // Checks if the first part of the page url is a blocked site
+            //         chrome.tabs.update(tabId, {url: "chrome://newtab"});
+            //     }
+            // }
+            var validUrlBeginnings = ["https://www.youtube.com/watch?v=", "https://www.youtube.com/playlist?list=", "https://www.youtube.com/results?search_query="];
+            
+            var isOnValidYTSite = false;
+            for(var i = 0; i < validUrlBeginnings.length; i++){
+                if(tab.url.slice(0, validUrlBeginnings[i].length) == validUrlBeginnings[i]){
+                    isOnValidYTSite = true;
+                }
+            }
+            if(!isOnValidYTSite){
+                chrome.tabs.update(tabId, {url: "https://www.youtube.com/results?search_query="});
             }
         }
     }
